b859703017989ad28262306fbb68cc24121bf814
Angel Echavarria <angel@interaction.cr>
2018-11-10 19:16:56 -0600

    update docs

---
+++ docs/precache-manifest.475a43b58b287539d74cac0ace9b5391.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3bc0dea57b9272aa3bd",
+    "url": "/static/js/main.e3bc0dea.chunk.js"
+  },
+  {
+    "revision": "16444d56ad9daab07dd9",
+    "url": "/static/js/1.16444d56.chunk.js"
+  },
+  {
+    "revision": "e3bc0dea57b9272aa3bd",
+    "url": "/static/css/main.bb88ba31.chunk.css"
+  },
+  {
+    "revision": "2266782317ecc4ad18de616d72f22343",
+    "url": "/index.html"
+  }
+];
